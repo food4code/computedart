dedd7168fbe6440d6b3dfbb277d1a6ec9f12591a
Eli Spizzichino <scrivi@gmail.com>
2013-02-19 22:36:10 +0100

    added admin

--- app/io_slides/views.py
+++ app/io_slides/views.py
@@ -1,3 +1,4 @@
+import shutil
 from PIL import Image
 from django.http import HttpResponse
 from django.shortcuts import redirect
@@ -16,42 +17,15 @@ from django.core.files import File
 def upload_list():
     image_dir = os.listdir(os.path.join(settings.MEDIA_ROOT ,'uploads'))
 
-MAXWIDTH = 980
-MAXHEIGHT = 600
-DELTA = 50
-#                                               Galleria.objects.get(pk=2).delete()
 
 
-def slides_write(request, dir_name):
-
-    cdir = os.path.dirname(os.path.abspath(__file__))
-    image_dir = os.path.join(settings.MEDIA_ROOT ,'uploads', dir_name)
-    template_file = os.path.join(cdir, 'static', dir_name + '.html')
-    image_abs_list = sorted( glob.glob( os.path.join(image_dir , '*.jpg' )) )
-
+def make_thumb(image_abs_list, image_dir):
+    MAXWIDTH = 980
+    MAXHEIGHT = 600
+    DELTA = 50
     thumb_dir = os.path.join(image_dir, settings.THUMBNAILS_DIR_NAME)
     thumb_list = []
 
-    gal = Galleria.objects.filter(title=dir_name)
-    if not gal:
-        zip_name = os.path.join(settings.MEDIA_ROOT, dir_name+'.zip')
-        zip = zipfile.ZipFile(zip_name, 'w')
-        for image_name in image_abs_list:
-            zip.write(image_name, os.path.basename(image_name))
-        zip.close()
-        gal = Galleria.objects.create(title=dir_name, zip_import=zip_name)
-        gal.save()
-
-
-#        print list(gal.images.all())
-#        for root, dirs, files in os.walk(dir_name):
-
-#        for image_name in image_abs_list:
-#            image_base = os.path.basename(image_name)
-#            fh = open(image_name)
-#            gal_image = GalleriaImage(gallery=gal)
-#            gal_image.file.save(image_name, File(fh))
-#            gal_image.save()
     for image_name in image_abs_list:
         image_base = os.path.basename(image_name)
 
@@ -72,26 +46,59 @@ def slides_write(request, dir_name):
             print "Processing", thumb_image
             thumb_image = thumbnail(image_name, *size)
         #else:   print "skipping", thumb_image
-        thumb_image = thumb_image.split('/static/')[1]
-        thumb_list.append(thumb_image)
+        thumb_list.append(os.path.basename(thumb_image))
 
-#    print thumb_list
+        ### Move original file
+        orig_dir = os.path.join(image_dir, 'orig')
+        if not os.path.exists(orig_dir):
+            os.makedirs(orig_dir)
+        shutil.move(image_name, orig_dir)
+
+    return thumb_list
+
+def slides_write(request, dir_name):
+
+    cdir = os.path.dirname(os.path.abspath(__file__))
+    image_dir = os.path.join(settings.MEDIA_ROOT ,'uploads', dir_name)
+    template_file = os.path.join(cdir, 'static', dir_name + '.html')
+    image_abs_list = sorted( glob.glob( os.path.join(image_dir , '*.jpg' )) )
+    thumb_dir = os.path.join(image_dir, settings.THUMBNAILS_DIR_NAME)
+
+    gal = Galleria.objects.filter(title=dir_name)
+    image_list = []
+    if not gal:
+        image_list = make_thumb(image_abs_list, image_dir)
+        zip_name = os.path.join(settings.MEDIA_ROOT, dir_name+'.zip')
+        zip = zipfile.ZipFile(zip_name, 'w')
+        for im in glob.glob( os.path.join(thumb_dir , '*.jpg' )):
+            zip.write(im, os.path.basename(im))
+        zip.close()
+        gal = Galleria.objects.create(title=dir_name, zip_import=zip_name)
+        gal.save()
+
+    else:
+        print "*****************************"
+        ### TODO: check for new files
+        gal = Galleria.objects.get(title=dir_name)
+
+        for gi in gal.images.all():
+            image_list.append(gi.file.filename)
+
+    image_list = map(lambda img: os.path.join(image_dir.split('/static/')[1], img), image_list )
+
+    print image_list
 
     t = loader.get_template('dtemplate.html')
     c = Context({
         'dir_name': dir_name,
-        'file_list': thumb_list
+        'file_list': image_list
     })
     template = t.render(c)
-    #print template
     f = codecs.open(template_file, "w", "utf-8")
     f.write(template)
-#    f.write()
     f.close()
-    #management.call_command('collectstatic', interactive=False)
 
     return redirect("/static/" + dir_name + '.html')
-    #return HttpResponse(template)
 
 def slides_send(request):
     # Create the HttpResponse object with the appropriate CSV header.
